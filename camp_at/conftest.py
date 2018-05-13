29f04cda8c490c36617d7840fdebc679ea45e3f0
qaivanpleten <chesteslav@gmail.com>
2018-05-13 18:39:49 +0300

    "the first one"

---
+++ camp_at/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def fixture_webdriver() -> webdriver:
+    driver = webdriver.Chrome(
+        "C:\\Users\Владелец\PycharmProjects\chromedriver")  # "C:\\Users\Владелец\PycharmProjects\chromedriver" '/home/user/chromedriver'
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    yield driver
+    driver.quit()
